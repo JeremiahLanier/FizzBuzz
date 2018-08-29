c5485f65e5a0f2e076be73b8caba8627de5a140e
Jeremiah R Lanier <remylanier91@gmail.com>
2018-08-29 13:14:37 -0400

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+for num in range(1, 101):
+    if num % 15 is 0:
+        print("FizzBuzz")
+    elif num % 3 is 0:
+        print("Fizz")
+    elif num % 5 is 0:
+        print("Buzz")
+    else:
+     print(num)
+
+for num in range(1,101):
+    if num % 10 is 0:
+        print("Python")
+    elif num % 5 is 0:
+        print("Java")
+    elif num % 2 is 0:
+        print("C++")
+    else:
+        print(num)
